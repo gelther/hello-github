d2e95611aec47b9f3d48934513b82caf8a7bb292
gelther <jeroen.sormani@gmail.com>
2022-06-22 15:18:09 +0200

    Update

--- bash.sh
+++ bash.sh
@@ -2,6 +2,6 @@ dates=( "2022/10/1" "2022/11/1" )
 
 for d in "${dates[@]}"
 do
-	git commit --amend --date="$i"
-	echo "$?"
+	echo Committing "$d"
+	git commit --amend --date="$d" --no-edit --quiet
 done
